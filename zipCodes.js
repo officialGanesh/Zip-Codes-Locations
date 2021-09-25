f075db6e5943cb129736912748cd09e6eeae706b
Ganesh <ganuofficial2795@gmail.com>
2021-09-25 19:52:58 +0530

    Alert message and countries

--- zipCodes.js
+++ zipCodes.js
@@ -1,1 +1,40 @@
-console.log("ZipCodes 🚀");
+console.log("ZipCodes Locations 🚀");
+
+class UI{
+
+    static showAlert(bootstrapColour,msg){
+        let element = document.createElement('div');
+        element.classList = `alert alert-${bootstrapColour}`;
+        element.innerHTML = msg;
+        
+        let cont = document.querySelector('.container');
+        let title = document.querySelector('#title');
+
+        // show alert message
+        cont.insertBefore(element,title);
+
+        // Remove alert message
+        setTimeout(()=>{document.querySelector('.alert').remove()},1500);
+
+    };
+
+    static variousCountryOptions(){
+
+        let fetchData = fetch('/countries.json').then(res=>{return res.json()}).then(data=>{
+            
+            let selectCountry = document.querySelector('#selectCountry');
+
+            data.forEach(e => {
+                let elem = document.createElement('option');
+                elem.value = e.Code;
+                elem.innerHTML = e.Country;
+                selectCountry.append(elem);
+                
+            });
+        });
+        
+    }; 
+
+};
+
+
